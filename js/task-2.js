59f3d24fad95b8d68e43af10b415e11318ebb0ae
EZH1809 <stellage.ua@gmail.com>
2023-12-07 20:14:05 +0200

    add task2

---
+++ js/task-2.js
@@ -0,0 +1,23 @@
+//VAR1
+// function getShippingMessage(country, price, deliveryFee) {
+//     const totalPrice = price + deliveryFee;
+//     console.log(`Shipping to ${country} will cost ${totalPrice} credits`)
+// }
+// console.log(getShippingMessage("Australia", 120, 50));
+// console.log(getShippingMessage("Germany", 80, 20));
+// console.log(getShippingMessage("Sweden", 100, 20));
+
+
+
+
+//VAR2
+function getShippingMessage(country, price, deliveryFee) {
+    const totalPrice = price + deliveryFee;
+    return`Shipping to ${country} will cost ${totalPrice} credits`
+}
+let message = getShippingMessage("Australia", 120, 50);
+console.log(message);
+message = getShippingMessage("Germany", 80, 20);
+console.log(message);
+message = getShippingMessage("Sweden", 100, 20);
+console.log(message);
